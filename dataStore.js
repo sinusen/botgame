a1bf8e4eec0ca1b9558a62822f747e7cf3e8c73e
Sinu Sen <sen_sinu@yahoo.com.au>
2021-01-02 18:36:49 +1100

    DB Connection String Refactor

--- dataStore.js
+++ dataStore.js
@@ -1,11 +1,7 @@
 const { Client } = require("pg");
 
 config = {
-  host: process.env.RDS_HOSTNAME,
-  port: process.env.RDS_PORT,
-  database: process.env.RDS_DB_NAME,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
+  connectionString: process.env.DB_CONNECTION_STRING,
   max: 20,
   connectionTimeoutMillis: 5000,
   idleTimeoutMillis: 30000,
